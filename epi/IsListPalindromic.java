4ca67ef55ec44fb7593b66bba61d638a53e08bf2
Weichun Hung <weichunhung@moneydj.com>
2020-12-05 21:51:14 +0800

    8.9-8.11

--- epi/IsListPalindromic.java
+++ epi/IsListPalindromic.java
@@ -5,8 +5,54 @@ public class IsListPalindromic {
   @EpiTest(testDataFile = "is_list_palindromic.tsv")
 
   public static boolean isLinkedListAPalindrome(ListNode<Integer> L) {
-    // TODO - you fill in here.
+    /*
+      Edge case would be null or odd or even len
+
+      Use two pointer to find center point, then reverse the first part
+
+      Compare the reversed first part with the following one, return true if no val difference
+
+      T O(N) S O(1)
+     */
+    if(L == null) {
+      return true;
+    }
+
+    ListNode slow = L, fast = L;
+    /*
+     1, 2, 3
+     1, 2, 3, 4
+     1, 2, 3, 4, 5
+     */
+    while(fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+    ListNode<Integer> first = L, second = reverse(slow);
+    while(first != null && second != null) {
+      if(first.data.intValue() != second.data.intValue()) {
+        return false;
+      }
+
+      first = first.next;
+      second = second.next;
+    }
+
     return true;
+  }
+
+  private static ListNode reverse(ListNode node) {
+    ListNode cur = node, prev = null;
+
+    while(cur != null) {
+      ListNode tmp = cur.next;
+      cur.next = prev;
+      prev = cur;
+      cur = tmp;
+    }
+
+    return prev;
   }
 
   public static void main(String[] args) {
--- epi/ListCyclicRightShift.java
+++ epi/ListCyclicRightShift.java
@@ -6,8 +6,44 @@ public class ListCyclicRightShift {
 
   public static ListNode<Integer> cyclicallyRightShiftList(ListNode<Integer> L,
                                                            int k) {
-    // TODO - you fill in here.
-    return null;
+    //T O(N) S O(1)
+    if(L == null) {
+      return null;
+    }
+
+    int len = 0;
+    ListNode cur = L;
+    //Find length, T O(N)
+    while(cur != null) {
+      len++;
+      cur = cur.next;
+    }
+
+    k %= len;
+    if(k == 0) {
+      return L;
+    }
+
+    //Transfer from right shift to counter clock shift
+    k = len - k;
+    int cnt = 1;
+    ListNode<Integer> dummy = new ListNode(-1, L), nEd = null, ed = null;
+    cur = dummy.next;
+    //T O(N)
+    while(cur != null) {
+      if(cnt == k) {
+        nEd = cur;
+      }
+      ed = cur;
+      cur = cur.next;
+      cnt++;
+    }
+
+    dummy.next = nEd.next;
+    nEd.next = null;
+    ed.next = L;
+
+    return dummy.next;
   }
 
   public static void main(String[] args) {
