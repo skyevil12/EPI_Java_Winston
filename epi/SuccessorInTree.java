0b567adf27617ac62df16ea0d9fe7f5ae8d633e1
Weichun Hung <weichunhung@moneydj.com>
2020-12-30 10:11:42 +0800

    10.10-10.12

--- epi/SuccessorInTree.java
+++ epi/SuccessorInTree.java
@@ -6,8 +6,31 @@ import epi.test_framework.TimedExecutor;
 public class SuccessorInTree {
 
   public static BinaryTree<Integer> findSuccessor(BinaryTree<Integer> node) {
-    // TODO - you fill in here.
-    return null;
+    /*
+      Check if current node has right child
+      Y-> find left most child of cur right child, or return the right child directly
+      N-> find if the parent is the left child of grand parent and return the grand parent
+      T O(h) h logN-N S O(1)
+    */
+    if(node == null) {
+      return null;
+    }
+
+    if(node.right != null) {
+      node = node.right;
+      while(node.left != null) {
+        node = node.left;
+      }
+      return node;
+    }
+
+    BinaryTree<Integer> parent = node.parent;
+    while(parent != null && parent.left != node) {
+      node = parent;
+      parent = parent.parent;
+    }
+
+    return parent;
   }
   @EpiTest(testDataFile = "successor_in_tree.tsv")
   public static int findSuccessorWrapper(TimedExecutor executor,
--- epi/TreeWithParentInorder.java
+++ epi/TreeWithParentInorder.java
@@ -2,13 +2,32 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.List;
 public class TreeWithParentInorder {
   @EpiTest(testDataFile = "tree_with_parent_inorder.tsv")
 
   public static List<Integer> inorderTraversal(BinaryTree<Integer> tree) {
-    // TODO - you fill in here.
-    return null;
+    /*
+      T O(N) S O(1) except output space
+     */
+    List<Integer> rtList = new ArrayList<>();
+    if(tree == null) {
+      return rtList;
+    }
+    //Find the left most
+    BinaryTree<Integer> next = tree;
+    while(next.left != null) {
+      next = next.left;
+    }
+
+    rtList.add(next.data);
+    //Use successor function to add rt
+    while((next = SuccessorInTree.findSuccessor(next)) != null) {
+      rtList.add(next.data);
+    }
+
+    return rtList;
   }
 
   public static void main(String[] args) {
