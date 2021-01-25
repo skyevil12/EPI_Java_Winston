0ecd1a0e4e502e7f22c18b63a4c58bb38df5e688
Weichun Hung <weichunhung@moneydj.com>
2021-01-25 17:32:35 +0800

    11.1-11.3

--- epi/SortedArraysMerge.java
+++ epi/SortedArraysMerge.java
@@ -2,14 +2,37 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
-import java.util.List;
+import java.util.*;
+
 public class SortedArraysMerge {
   @EpiTest(testDataFile = "sorted_arrays_merge.tsv")
 
   public static List<Integer>
   mergeSortedArrays(List<List<Integer>> sortedArrays) {
-    // TODO - you fill in here.
-    return null;
+    /*
+      (3,5,7), (0,6), and (0,6, 28)
+     */
+    Queue<Queue<Integer>> sorted = new PriorityQueue<>(new Comparator<Queue<Integer>>() {
+      @Override
+      public int compare(Queue<Integer> o1, Queue<Integer> o2) {
+        return o1.peek() - o2.peek();
+      }
+    });
+
+    for(List<Integer> sortedArray : sortedArrays) {
+      Queue<Integer> queue = new ArrayDeque<>(sortedArray);
+      sorted.offer(queue);
+    }
+
+    List<Integer> rtList = new ArrayList<>();
+    while(!sorted.isEmpty()) {
+      Queue<Integer> minQ = sorted.poll();
+      rtList.add(minQ.poll());
+      if(!minQ.isEmpty()) {
+        sorted.offer(minQ);
+      }
+    }
+    return rtList;
   }
 
   public static void main(String[] args) {
