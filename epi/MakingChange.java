d8863f8495370728371ae054396aed343c79f25e
Winston_Hung <skyevil12@gmail.com>
2021-03-24 16:36:36 +0800

    17.1

--- epi/MakingChange.java
+++ epi/MakingChange.java
@@ -5,8 +5,13 @@ public class MakingChange {
   @EpiTest(testDataFile = "making_change.tsv")
 
   public static int changeMaking(int cents) {
-    // TODO - you fill in here.
-    return 0;
+    int[] coins = {1, 5, 10, 25, 50, 100};
+    int rt = 0;
+    for(int i = coins.length - 1; i >= 0; i--) {
+      rt += cents / coins[i];
+      cents %= coins[i];
+    }
+    return rt;
   }
 
   public static void main(String[] args) {
